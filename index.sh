5f40bce39008605ee78a21497781722f21485485
gul5oy <51869321+gul5oy@users.noreply.github.com>
2019-06-16 06:02:32 +0300

    Add files via upload

---
+++ index.sh
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?><memo Version="1.0">
+<header>
+<meta title="index.sh"/>
+<meta favourite="false"/>
+<meta uuid="e401bf2a-0ddc-a8bf-0000-016b5e312a0e"/>
+<meta createdTime="1560653408782"/>
+</header>
+<contents>
+<content>&lt;p value="memo2" &gt;pkg install bash -y&lt;/p&gt;&lt;p&gt;pkg install cowsay -y&lt;/p&gt;&lt;p&gt;pkg install figlet -y&lt;/p&gt;&lt;p&gt;pip2 install toilet&lt;/p&gt;&lt;p&gt;pkg install toilet -y&lt;/p&gt;&lt;p&gt;pip2 install lolcat&lt;/p&gt;&lt;p&gt;pkg install curl -y&lt;/p&gt;&lt;p&gt;clear&lt;/p&gt;&lt;p&gt;bash cache&lt;/p&gt;</content>
+<xml_content>&lt;todolist&gt;&lt;todolist_item todo_item_state="false" todo_item_type="0" todo_item_content="&amp;lt;p&amp;gt;pkg install bash -y&amp;lt;/p&amp;gt;&amp;lt;p&amp;gt;pkg install cowsay -y&amp;lt;/p&amp;gt;&amp;lt;p&amp;gt;pkg install figlet -y&amp;lt;/p&amp;gt;&amp;lt;p&amp;gt;pip2 install toilet&amp;lt;/p&amp;gt;&amp;lt;p&amp;gt;pkg install toilet -y&amp;lt;/p&amp;gt;&amp;lt;p&amp;gt;pip2 install lolcat&amp;lt;/p&amp;gt;&amp;lt;p&amp;gt;pkg install curl -y&amp;lt;/p&amp;gt;&amp;lt;p&amp;gt;clear&amp;lt;/p&amp;gt;&amp;lt;p&amp;gt;bash cache&amp;lt;/p&amp;gt;"/&gt;&lt;/todolist&gt;</xml_content>
+</contents>
+</memo>
